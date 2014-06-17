a3f6f9cb89696aa543a23df1e61a5a16837b27e7
CreativeOutbreak <pete@creativeoutbreak.co.uk>
2014-06-17 18:53:25 +0100

    First commit of co_patternlab.

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    // Configuration 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            patternlab: {
+                command: "php core/builder.php -wpr"
+            },
+        },
+        compass: {                    // Task
+            dist: {                   // Target
+                options: {            // Target options
+                    sassDir: 'source/scss',
+                    cssDir: 'public/css',
+                    environment: 'production'
+                }
+            }
+        },
+        watch: {
+            html: {
+                files: ['source/_patterns/**/*.mustache', 'source/_patterns/**/*.json', 'source/_data/*.json'],
+                tasks: ['shell:patternlab'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['source/scss/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+           }
+
+        }
+    });
+
+    // Plugins
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Tasks
+    grunt.registerTask('default', ['watch', 'compass']);
+    };
